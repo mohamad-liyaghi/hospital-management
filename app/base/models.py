b13c1b6af726dec837be0dc81e7dbde4b89413fb
Ml06py <liaghimohamad69@gmail.com>
2022-04-28 17:06:47 +0430

    Admins app added

--- app/base/models.py
+++ app/base/models.py
@@ -32,6 +32,12 @@ class BaseUser(AbstractUser):
         admin = ("ad","admin")
         doctor = ("do","doctor")
     user_status = models.CharField(max_length=2,choices=status.choices,default=status.patient,blank=True,null=True)
+    class admin_status(models.TextChoices):
+        requested = ("re", "requested")
+        didnt_requested = ("dr", "didnt_requested")
+        accepted = ("ac", "accepted")
+        declined = ("de", "declined")
+    admin_stat = models.CharField(max_length=2, choices=admin_status.choices, default=admin_status.didnt_requested, blank=True, null=True)
     # permissions
     add_hospital = models.BooleanField(default=False)
     add_doctor = models.BooleanField(default=False)
