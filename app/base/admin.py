579454233907e187564d4ac0b774a224a954f5cd
Ml06py <liaghimohamad69@gmail.com>
2022-05-22 19:22:11 +0430

    Doctor/Admin model added

--- app/base/admin.py
+++ app/base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import BaseUser
+from .models import BaseUser, Admin, Doctor
 # Register your models here.
 admin.site.register(BaseUser)
+admin.site.register(Admin)
+admin.site.register(Doctor)
--- app/base/models.py
+++ app/base/models.py
@@ -45,3 +45,28 @@ class BaseUser(AbstractUser):
     add_admin = models.BooleanField(default=False)
     def __str__(self):
         return self.first_name
+
+
+class Doctor(models.Model):
+    class doctor_status(models.TextChoices):
+        requested = ("re", "requested")
+        didnt_requested = ("dr", "didnt_requested")
+        accepted = ("ac", "accepted")
+        declined = ("de", "declined")
+        
+    applier = models.ForeignKey(BaseUser,on_delete=models.CASCADE,blank=True, related_name="applier")
+    doctor_id = models.PositiveIntegerField(blank=True,unique=True,null=True)
+    hospital_to_request = models.ManyToManyField(Hospital,blank=True)
+    more_info = models.TextField(blank=True,null=True)
+    doctor_status = models.CharField(max_length=2, choices=doctor_status.choices, default=doctor_status.didnt_requested,blank=True,null=True)
+
+class Admin(models.Model):
+    class admin_status(models.TextChoices):
+        requested = ("re", "requested")
+        didnt_requested = ("dr", "didnt_requested")
+        accepted = ("ac", "accepted")
+        declined = ("de", "declined")
+
+    applier = models.ForeignKey(BaseUser,on_delete=models.CASCADE,blank=True, related_name="admin_applier")
+    admin_stat = models.CharField(max_length=2, choices=admin_status.choices, default=admin_status.didnt_requested, blank=True, null=True)
+    admin_description = models.TextField(blank=True,null=True)
